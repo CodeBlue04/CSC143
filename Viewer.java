195874d8be6c23b011e2d51f7e50f8c3beac2e63
CodeBlue04 <colbyeaston@gmail.com>
2018-02-03 11:28:43 -0800

    Update Viewer.java

--- Viewer.java
+++ Viewer.java
@@ -1,5 +1,25 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
 public class Viewer extends JPanel implements View {
 
+	protected DrawingModel model;
+	
+	protected Graphics g;
+	
+	public Viewer() {
+		setBackground(Color.WHITE);
+	}
+	
+	public void paintComponent(Shape s) {
+		if (s != null) {
+			for (DrawingModel model; model.getShapes(s.draw(g)) {
+				s.update(model);
+			}
+		}
+		
+		
+	}
 }
