74650d4c99279a82dee799adc9e4130828338de7
CodeBlue04 <colbyeaston@gmail.com>
2018-02-03 11:27:39 -0800

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -1,4 +1,30 @@
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class MainClass {
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				createGUI();
+				
+			}
+		});
+		
+	}
+	
+	public static void createGUI() {
+		JFrame frame = new JFrame("Homework 3");
+		frame.setSize(800, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//DrawingModel
+		DrawingModel model = new DrawingModel();
+		Viewer view = new Viewer();
+		model.ad
+		
+	}
 
 }
