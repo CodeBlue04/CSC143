799585f96cfc5c236503cd27aced2aa3514d060b
CodeBlue04 <colbyeaston@gmail.com>
2018-02-03 11:27:15 -0800

    Update HShape.java

--- HShape.java
+++ HShape.java
@@ -1,7 +1,9 @@
 import java.awt.Color;
+import java.awt.Graphics;
 
 public class HShape extends AbstractShape{
 
+	DrawingModel model;
 	
 	/**
 	 * Constructor for H Shape
@@ -17,5 +19,21 @@ public class HShape extends AbstractShape{
 	public Shape model(Shape s) {
 		Shape H = s;
 		return H;
+	}
+	
+	public void draw(Graphics g) {
+		g.setColor(Color.BLUE); // turns the character blue
+		int[] widthHeight = model.getCoord();
+		int height = widthHeight[1];
+		int width = widthHeight[0];
+		int squareSize = width / 3;
+		for (int i = 0; i<3;i++) {
+			for (int j=0;j<3;j++) {
+				g.drawRect(width+(j*width), height-(i*height), squareSize, squareSize);
+				if (i != 1 ||(i==1 && j==1)) {
+				g.fillRect(width+(j*width), height-(i*height), squareSize, squareSize);
+			} 
+			}
+		}
 	}
 }
