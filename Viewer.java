0f00edc773803224b3607e64b7c98f4add37303f
CodeBlue04 <colbyeaston@gmail.com>
2018-02-04 11:55:44 -0800

    Update Viewer.java

--- Viewer.java
+++ Viewer.java
@@ -5,21 +5,26 @@ import javax.swing.JPanel;
 
 public class Viewer extends JPanel implements View {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	protected DrawingModel model;
 	
 	protected Graphics g;
-	
-	public Viewer() {
-		setBackground(Color.WHITE);
+
+	public void update(DrawingModel model) {
+		this.model = model; //repaints this DrawingModel object 
+		repaint();
 	}
 	
-	public void paintComponent(Shape s) {
+	public void paintComponent(Shape s) { //paints all components in the Arraylist of shapes
+		super.paintComponents(g);
 		if (s != null) {
-			for (DrawingModel model; model.getShapes(s.draw(g)) {
-				s.update(model);
+			for (Shape shape: model.getShapes(g)) {
+				shape.draw(g);
 			}
 		}
-		
-		
 	}
 }
