18493aec59a3549e116387b0eecedb85b4c605c9
CodeBlue04 <colbyeaston@gmail.com>
2018-02-04 11:54:00 -0800

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -1,30 +1,36 @@
+import java.awt.Color;
+
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 public class MainClass {
-	
+
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			
-			@Override
-			public void run() {
+
 				createGUI();
-				
-			}
-		});
-		
+
 	}
-	
+
 	public static void createGUI() {
 		JFrame frame = new JFrame("Homework 3");
 		frame.setSize(800, 600);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		//DrawingModel
+		frame.setBackground(Color.BLACK);
+
+
+		// DrawingModel
 		DrawingModel model = new DrawingModel();
 		Viewer view = new Viewer();
-		model.ad
 		
+		Shape fib = new FibonacciSquare(400,400, Color.RED, 1, 200);
+		Shape h = new HShape(200,200,Color.BLUE,150);
+		model.addShape(fib);
+//		model.addShape(h);
+		model.addView(view);
+		frame.add(view);
+		
+		
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
