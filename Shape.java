0a2c43a3ecff6308a8fc98f4a4c45ef29b4f5aee
CodeBlue04 <colbyeaston@gmail.com>
2018-02-04 11:54:29 -0800

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -2,12 +2,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public interface Shape {
-	public void AbstractShape(int x, int y, Color c, int size);
-	
-	public Shape deepCopy();
-	
-	public void draw(Graphics g);
-	
-	public void update(DrawingModel model);
-}
 
+	public void draw(Graphics g);
+
+}
