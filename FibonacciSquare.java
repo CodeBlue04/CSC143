2ba2f38af1e106c5b69fc83e2681786f8e6cb603
CodeBlue04 <colbyeaston@gmail.com>
2018-02-04 11:52:44 -0800

    Update FibonacciSquare.java

--- FibonacciSquare.java
+++ FibonacciSquare.java
@@ -2,12 +2,13 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.util.Random;
 
-public class FibonacciSquare extends AbstractShape{
-	
-	private Random rand = new Random(); 
-	
+public class FibonacciSquare extends AbstractShape {
+
+//	private Random rand = new Random();
+
 	/**
 	 * Constructor for Fibonacci Square shape
+	 * 
 	 * @param x
 	 * @param y
 	 * @param c
@@ -17,29 +18,53 @@ public class FibonacciSquare extends AbstractShape{
 		super(x, y, Color.RED, size);
 		this.quadrant = quadrant;
 	}
-	
+
 	public int fibNum(int n) {
-		if (n <=1) {
+		if (n <= 1) {
 			return n;
 		} else {
-			return fibNum(n-1) + fibNum(n-2);
+			return fibNum(n - 1) + fibNum(n - 2);
 		}
 	}
-	
+
+	public Shape deepCopy() {
+		int x = this.x;
+		int y = this.y;
+		Color c = this.c;
+		int quadrant = this.quadrant;
+		int size = this.size;
+		return new FibonacciSquare(x, y, c, quadrant, size);
+	}
+
 	public void draw(Graphics g) {
-		g.setColor(Color.RED);
-		int xCoord = rand.nextInt(800);
-		int yCoord = rand.nextInt(800);
-		int widthHeight = rand.nextInt(50);
-		
-		g.drawRect(xCoord, yCoord, widthHeight, widthHeight);
-		
-		//Quadrant 1
-		g.drawArc()
-		//Quadrant 2
-		
-		//Quadrant 3
-		
-		//Quadrant 4
+		g.setColor(this.c);
+
+		// g.drawRect(xCoord, yCoord, widthHeight, widthHeight);
+		switch (quadrant) {
+		// Quadrant 1
+		case 0:
+			g.drawRect(x + (size / 2), y, size, size);
+			g.drawArc(x, y, size, size, 0, 90);
+			break;
+
+		// Quadrant 2
+		case 1:
+			g.drawRect(x + (size / 2), y, size, size);
+			g.drawArc(x, y, size, size, 90, 180);
+			break;
+
+		// Quadrant 3
+		case 2:
+			g.drawRect(x + (size / 2), y, size, size);
+			g.drawArc(x, y, size, size, 180, 270);
+			break;
+
+		// Quadrant 4
+		case 3:
+			g.drawRect(x + (size / 2), y, size, size);
+			g.drawArc(x, y, size, size, 270, 0);
+			break;
+
+		}
 	}
 }
