e91a1d30b82198eb212ca9a039fb42e024a8f9d3
CodeBlue04 <colbyeaston@gmail.com>
2018-02-05 12:04:38 -0800

    Update HShape.java

--- HShape.java
+++ HShape.java
@@ -33,6 +33,7 @@ public class HShape extends AbstractShape {
 	 * @param g
 	 */
 	public void draw(Graphics g) {
+		int size = this.size/3;
 		for (int i = 0; i < 3; i++) {// nested for loops to create H shape, i is vertical
 			for (int j = 0; j < 3; j++) {
 				if (j != 1 || (i == 1 && j == 1)) {
