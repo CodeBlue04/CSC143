c10141f454857701d9750d1ea47454e14f8c3da3
CodeBlue04 <colbyeaston@gmail.com>
2018-02-05 12:03:29 -0800

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -1,35 +1,44 @@
 import java.awt.Color;
+import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 public class MainClass {
+	
+	private static Random rand = new Random();
 
 	public static void main(String[] args) {
 
-		createGUI();
-
+				createGUI();//runs entire program
 	}
 
 	public static void createGUI() {
-		JFrame frame = new JFrame("Homework 3");
-		frame.setSize(800, 600);
-		frame.setBackground(Color.YELLOW);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JFrame frame = new JFrame("Homework 3");//Title of frame
+		frame.setSize(800, 600);//Dimensions of frame
+		frame.setBackground(Color.YELLOW);//Sets background color
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//End condition
 
 		// DrawingModel
 		DrawingModel model = new DrawingModel();
 		Viewer view = new Viewer();
-
-		// Shape fib = new FibonacciSquare(200,200, 10, Color.RED, 0);
-		Shape h = new HShape(400, 400, 15, Color.BLUE);
-		model.addShape(h);
-
+		
 		model.addView(view);
 		frame.add(view);
-
-		frame.setVisible(true);
-
+		
+		for(int i = 0; i <10; i++) { // for loop to create shapes. Similar to example, but with different math
+			int size = rand.nextInt(20) + 20;//put first to contain shapes within frame
+			int x = rand.nextInt(frame.getWidth() - (20+size));//Ensures that x coordinate leaves entire shape in frame
+			int y = rand.nextInt(frame.getHeight() -(20+size));//Ensures that y coordinate leaves entier shape in frame
+			int s = rand.nextInt(2);//Decided which shape is created using random number
+			if (s == 0) {
+				model.addShape(new HShape(x,y,size, Color.BLUE));
+			} else {
+				int quadrant = rand.nextInt(4)+1;//Quadrant needs to be on interval (1,4), so 1+(0,3) creates correct int range
+				model.addShape(new FibonacciSquare(x, y, size/3 ,Color.RED, quadrant));
+			}
+		}
+		frame.setVisible(true); //Put at bottom to make frame visible
 	}
 
 }
