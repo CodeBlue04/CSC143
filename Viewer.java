a4cafd65bc7a4562cbc07ba7823981f84c1c32a8
CodeBlue04 <colbyeaston@gmail.com>
2018-02-04 18:35:43 -0800

    Update Viewer.java

--- Viewer.java
+++ Viewer.java
@@ -18,10 +18,10 @@ public class Viewer extends JPanel implements View {
 		this.model = model; //repaints this DrawingModel object 
 		repaint();
 	}
-	
-	public void paintComponent(Shape s) { //paints all components in the Arraylist of shapes
+	@Override
+	public void paintComponent(Graphics g) { //paints all components in the Arraylist of shapes
 		super.paintComponents(g);
-		if (s != null) {
+		if (this != null) {
 			for (Shape shape: model.getShapes(g)) {
 				shape.draw(g);
 			}
