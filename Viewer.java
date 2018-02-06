c620b524700bc18ef0bfeb6d770504243504d0df
CodeBlue04 <colbyeaston@gmail.com>
2018-02-05 21:47:34 -0800

    Update Viewer.java

--- Viewer.java
+++ Viewer.java
@@ -1,8 +1,14 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
-
+/**
+ * Viewer class has methods to update the desired DrawingModel object,
+ * and another to draw all shapes in ArrayList of DrawingModel objects/shapes
+ * @author Colby Easton, Max Sutters
+ *
+ */
 public class Viewer extends JPanel implements View {
 
 	private static final long serialVersionUID = 1L;
@@ -24,7 +30,7 @@ public class Viewer extends JPanel implements View {
 	public void paintComponent(Graphics g) { // paints all components in the Arraylist of shapes
 		super.paintComponents(g);
 		if (this != null) {
-			for (Shape shape : model.getShapes(g)) {
+			for (Shape shape : model.getShapes(g)) {//for each shape in the ArrayList of shapes, draw that shape
 				shape.draw(g);
 			}
 		}
